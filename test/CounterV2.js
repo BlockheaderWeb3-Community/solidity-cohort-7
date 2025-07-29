4b5931c113ab3b100eee366b17a038f8a5690706
Haroldwonder <bossharoldjohn@gmail.com>
2025-07-29 15:39:09 +0100

    Add CounterV2 contract and full test suite

--- test/CounterV2.js
+++ test/CounterV2.js
@@ -17,38 +17,89 @@ describe("CounterV2 Test Suite", () => {
             const { counter } = await loadFixture(deployContract);
             expect(await counter.getCount()).to.equal(0);
         });
+        it("Should set the correct owner", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            expect(await counter.getOwner()).to.equal(owner.address);
+        });
     });
 
     describe("setCount", () => {
         it("Should allow owner to set count", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(5);
+            expect(await counter.getCount()).to.equal(5);
+        });
+
+        it("Should not allow non-owner to set count", async () => {
             const { counter, otherAccount } = await loadFixture(deployContract);
-            await expect( counter.connect(otherAccount).setCount(1)).to.be.revertedWith("You are unauthorised");
+            await expect(counter.connect(otherAccount).setCount(1)).to.be.revertedWith("You are unauthorised");
         });
 
         it("Should not allow count to be set at 0", async () => {
-            const { counter } = await loadFixture(deployContract);
-            await expect(counter.setCount(0)).to.be.revertedWith("Count must be greater than 0");
+            const { counter, owner } = await loadFixture(deployContract);
+            await expect(counter.connect(owner).setCount(0)).to.be.revertedWith("Count must be greater than 0");
         });
-    });  
-         
-    describe("getCount", () => {
-        it("Should allow owner to get count", async () => {
-            const [owner] = await ethers.getSigners();
-            const { counter } = await loadFixture(deployContract);
-            const count = await counter.connect(owner).getCount();
-            console.log("Count value:", count, "Type:", typeof count); // Debug
-            if (typeof count === "object" && count.toString) {
-            expect(count.toString()).to.equal("10"); // Try string comparison for BigNumber
-            } else {
-            expect(Number(count)).to.equal(10); // Fallback to number
-    }
     });
-}); 
 
-//     describe("getCount", () => {
-//         it("Should allow owner to get count", async () => {
-//         const [owner, otherAccount] = await ethers.getSigners();
-//         const { counter } = await loadFixture(deployContract);
-//         const count = await counter.connect(owner).getCount(); // Assign the result
-//         expect(count.toNumber()).to.equal(10); // Compare the resolved value (adjust for BigNumber)
-//         });
+    describe("increaseCountByOne", () => {
+        it("Should allow owner to increase count by one", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(2);
+            await counter.connect(owner).increaseCountByOne();
+            expect(await counter.getCount()).to.equal(3);
+        });
+
+        it("Should not allow non-owner to increase count", async () => {
+            const { counter, otherAccount } = await loadFixture(deployContract);
+            await expect(counter.connect(otherAccount).increaseCountByOne()).to.be.revertedWith("You are unauthorised");
+        });
+    });
+
+    describe("decreaseCountByOne", () => {
+        it("Should allow owner to decrease count by one", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(5);
+            await counter.connect(owner).decreaseCountByOne();
+            expect(await counter.getCount()).to.equal(4);
+        });
+
+        it("Should not allow non-owner to decrease count", async () => {
+            const { counter, otherAccount } = await loadFixture(deployContract);
+            await expect(counter.connect(otherAccount).decreaseCountByOne()).to.be.revertedWith("You are unauthorised");
+        });
+
+        it("Should allow decrease via callerCounterV2", async () => {
+        const { counter, owner, caller } = await loadFixture(deployContract);
+        await counter.connect(owner).setCount(3);
+  
+        });
+    });
+
+    describe("resetCount", () => {
+        it("Should allow owner to reset count to 0", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(7);
+            await counter.connect(owner).resetCount();
+            expect(await counter.getCount()).to.equal(0);
+        });
+
+        it("Should not allow reset if count is already 0", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await expect(counter.connect(owner).resetCount()).to.be.revertedWith("Cannot reset value , It's already at default");
+        });
+
+        it("Should not allow non-owner to reset count", async () => {
+            const { counter, otherAccount, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(2);
+            await expect(counter.connect(otherAccount).resetCount()).to.be.revertedWith("You are Unauthorised");
+        });
+    });
+
+    describe("getCount", () => {
+        it("Should return the correct count", async () => {
+            const { counter, owner } = await loadFixture(deployContract);
+            await counter.connect(owner).setCount(10);
+            expect(await counter.getCount()).to.equal(10);
+        });
+    });
+});
