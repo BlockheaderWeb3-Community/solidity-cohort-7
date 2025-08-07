fc078565aceef9e054b4a79bcb9476f1e464f7bf
passygigi <passyjoseph@gmail.com>
2025-08-07 14:00:01 +0100

    feat: assignmentMember

---
+++ src/AccessToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+contract AccessToken is ERC20{
+
+    address private owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Unauthorized User");
+        _;
+    }
+
+    constructor() ERC20("AccessToken", "ACS"){
+        owner = msg.sender;
+    }
+    
+    function mint(address to, uint256 _amount) onlyOwner external {
+        _mint(to, _amount);
+    }
+
+    function getOwner() external view returns(address) {
+        return owner;
+    }
+}
